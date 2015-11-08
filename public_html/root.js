1bd44c402de37a1e2c8202bff837448df9dd58fe
Sushi <Sushi@172.24.132.110>
2015-11-08 00:21:48 -0500

    HomeWork 5 Created the navigation Bar for the website

--- public_html/root.js
+++ public_html/root.js
@@ -5,10 +5,31 @@
  */
 
 var name = "Suraj Sharma";
-
+var url = "./data.json";
 var img = "http://www.stonybrook.edu/sb/images/titles/directions.jpg";
 
 window.onload = function(){
 document.getElementById("Banner_image").src = img;
 document.getElementById("Student_Name").innerHTML = name;
 }
+
+var obj;
+
+
+$.getJSON(url, function(data) {
+    
+    obj = data;
+      
+  document.getElementById("Page1").innerHTML = obj.Page1[0].title;
+  document.getElementById("Page2").innerHTML = obj.Page2[0].title;
+  document.getElementById("Page3").innerHTML = obj.Page3[0].title;
+  document.getElementById("Page4").innerHTML = obj.Page4[0].title;
+  document.getElementById("Page5").innerHTML = obj.Page5[0].title;
+  
+   function selectPage(){
+       
+   }
+    
+    
+    });
+    
